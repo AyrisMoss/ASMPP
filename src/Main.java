c001c519b5ce24c0996e1f7d715248b0559edae9
AyrisMoss <158268717+AyrisMoss@users.noreply.github.com>
2024-08-23 23:48:23 +0700

    done

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,15 @@
-import java.awt.desktop.OpenURIEvent;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
-
-    static List<Student> studentList = new ArrayList<>();
     public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-        int n ;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Quantity:");
-        n = scanner.nextInt();
-
-
-        for(int i = 1;i<=n;i++){
-            input();
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
-        System.out.println("IN DANH SACH SV:");
-        output();
-    }
-    public static void input()
-    {
-
-        System.out.println("Enter student information:");
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter Student's code:");
-        String code = scanner.nextLine();
-        System.out.println("Enter Student's name:");
-        String name = scanner.nextLine();
-        System.out.println("Enter Student's Grade:");
-        float grade = scanner.nextFloat();
-        Student student = new Student(code, name, grade);
-        studentList.add(student);
-    }
-    // In danh sach sinh vien
-    public static void output(){
-
-        for(Student student: studentList){
-
-            System.out.println(student.toString());
-        }
-    }
-    public static void removeByCode(String code)
-    {
-
-    }
-    public static  void sortByGradeDesc()
-    {
-
-    }
-    public static Student findByCodeOrName(String keyword){
-        return null;
-    }
-    public static List<Student> filterByGrade(float x){
-        return  null;
     }
 }
